73bace2f09c85e989da20c4dd9cefe204d637aba
Andrei Mititelu <andrei.mititelu@tradingcentral.com>
2023-08-09 21:00:07 +0300

    Added fields

---
+++ Parameters.java
@@ -0,0 +1,7 @@
+public class Parameters {
+    private String OS; // Operating System
+    private String gamePATH; // Path to game location
+    private String credentials;// User credentials
+    private int noLives; // Number of lives of the user
+
+}
