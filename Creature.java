8e12b2f58116e3a956e55050011f03ff2f2d5a33
RadutIulianDaniel <radut.iulian.daniel@gmail.com>
2023-08-09 21:06:59 +0300

    Created class Creature with abilities, nickname and score private fields

---
+++ Creature.java
@@ -0,0 +1,5 @@
+public class Creature {
+    private Abilities abilities;  //Class that represent the abilities of a creature(power, speed and agility)
+    private String nickname;  //nickname of the creature
+    private long score;  //Total score of the creature in game
+}
